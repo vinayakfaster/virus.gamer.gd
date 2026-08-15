f2819bee0263aae53cb612ec914deb5ff77c0f3b
vinayak shukla <65018895+vinayakfaster@users.noreply.github.com>
2026-08-16 04:11:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ $defaults = [
     'xSuperProperties' => $cfg['xSuperProperties'] ?? '',
     'installationId'   => $cfg['installationId'] ?? '',
     'maxMessages'      => $cfg['maxMessages'] ?? 5,
-    'delayPool'        => $cfg['delayPool'] ?? '30, 35, 36, 87, 45, 52, 96, 200, 300, 120, 15, 36, 10',
+    'delayPool'        => $cfg['delayPool'] ?? 30, 35, 36, 17, 45, 52, 96, 120, 15, 36, 10',
 ];
 ?>
 <!DOCTYPE html>
